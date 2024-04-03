d365d81de61563021c46f5ac2d49f76dd24c709c
Naishaminnah <155439876+Naishaminnah@users.noreply.github.com>
2024-04-03 16:30:57 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
-const express = require('express');
-const router = express();
+const {User} = require('./model/user');
 
-
-/////////user management
-
-
-router.get('/user', function(req,res){
-    console.log(req.body);
-    getUser(req.body);
-
-})
-router.post('/addUser', function(req,res){
-    console.log(re.body);
-    addUser
-})
-module.exports = router;
+function addUser(req,res){
+    try {
+    if(!req.body) throw Error('user data is not found');
+    const newUser = User.insert(req.body);
+    newUser.save();
+    if(newUser){
+        res.status(200).send(newUser);
+    }
+    }
+catch (error){
+    res.status(500).send(error.message);
+}
+}
+function getUser(req,res){
+    try{
+      const users = User.find();
+      if(users){
+        res.status(200).send(users);
+      }
+    }catch (error){
+        res.status(500).send(error.message);
+    }
+}
+module.exports = {
+    addUser,
+    getUser
+}
